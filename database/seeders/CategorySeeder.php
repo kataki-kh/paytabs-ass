635cb278ce8a04b8bbba1e45cb48dd6cda6c57c9
kataki <kataki.kh2017@gmail.com>
2023-06-01 20:57:12 +0300

    making db & seed data

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -13,5 +13,35 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         //
+        \App\Models\Category::factory()->create([
+            'id' => 1,
+             'name' => 'Category A',
+             'master_category_id'=>null,
+         ]);
+        \App\Models\Category::factory()->create([
+            'id' => 2,
+             'name' => 'Category B',
+             'master_category_id'=>null,
+         ]);
+        \App\Models\Category::factory()->create([
+            'id' => 3,
+             'name' => 'SUB B1',
+             'master_category_id'=>2,
+         ]);
+        \App\Models\Category::factory()->create([
+            'id' => 4,
+             'name' => 'SUB B2',
+             'master_category_id'=>2,
+         ]);
+        \App\Models\Category::factory()->create([
+            'id' => 5,
+             'name' => 'SUB SUB B2-1',
+             'master_category_id'=>4,
+         ]);
+        \App\Models\Category::factory()->create([
+            'id' => 6,
+             'name' => 'SUB SUB B2-1',
+             'master_category_id'=>4,
+         ]);
     }
 }
